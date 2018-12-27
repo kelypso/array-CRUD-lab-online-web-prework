0f4741b83185564edc2585a5b306fa78e37a4ffa
Kelsey Low <kklowlow@gmail.com>
2018-12-27 02:00:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  dogs = ['doxie', 'lab', 'boxer']
-  dogs << 'pitty'
+  array
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
